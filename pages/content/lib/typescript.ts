76c2c75afb6c16e82c1350fccd0d6105bd10b32b
Su-Yong <simssy2205@gmail.com>
2024-07-12 10:31:05 +0900

    fix(typescript): fix `typescriptlang.org` using `runOnReactPage`

--- pages/content/lib/typescript.ts
+++ pages/content/lib/typescript.ts
@@ -1,33 +1,9 @@
-import { createImage, runOnPage } from '@lib/utils';
+import { createImage, runOnPage, runOnReactPage } from '@lib/utils';
 
-runOnPage('www.typescriptlang.org', async () => {
-  let cleanUp: (() => void) | null = null;
-  queueMicrotask(() => {
+runOnReactPage('www.typescriptlang.org', async () => {
+  const logo = document.querySelector<HTMLAnchorElement>('#home-page-logo');
+  if (!logo) return;
 
-    const logo = document.querySelector<HTMLAnchorElement>('#home-page-logo');
-    if (!logo) return;
-
-    const observer = new MutationObserver(() => {
-      if (logo.firstElementChild?.tagName === 'IMG') return;
-
-      const newLogo = document.querySelector<HTMLAnchorElement>('#home-page-logo');
-      if (!newLogo) return;
-
-      observer.observe(newLogo, { childList: true });
-      cleanUp = () => observer.disconnect();
-
-      newLogo.style.width = 'fit-content';
-      newLogo.replaceChildren(createImage('TypeScript/TypeScript.png'));
-
-      console.log('mutation');
-    });
-    observer.observe(logo, { childList: true });
-    cleanUp = () => observer.disconnect();
-    console.log('replace');
-
-    logo.style.width = 'fit-content';
-    logo.replaceChildren(createImage('TypeScript/TypeScript.png'));
-  });
-
-  return () => cleanUp?.();
+  logo.style.width = 'fit-content';
+  logo.replaceChildren(createImage('TypeScript/TypeScript.png'));
 });
--- pages/content/lib/utils.ts
+++ pages/content/lib/utils.ts
@@ -1,21 +1,51 @@
 export const BASE_URL = 'https://raw.githubusercontent.com/SAWARATSUKI/KawaiiLogos/main/';
 export const toURL = (url: string) => `${BASE_URL}${url}`;
-export const runOnPage = (url: string, fn: () => Promise<(() => void) | void>) => {
+
+type Removable = (() => void) | void | Promise<(() => void) | void>;
+export const runOnPage = async (url: string, fn: () => Removable) => {
   let cleanUp: (() => void) | null = null;
-  console.log('runOnPage', window.location.host, url);
   if (window.location.host === url) {
-    fn().then((cleanUpFn) => cleanUpFn ? cleanUp = cleanUpFn : void 0);
+    const cleanUpFn = await fn();
+    if (cleanUpFn) cleanUp = cleanUpFn;
   }
 
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-expect-error
-  window.navigation.addEventListener('navigate', () => {
-    console.log('runOnPage', window.location.host, url);
+  window.navigation.addEventListener('navigate', async () => {
     cleanUp?.();
     if (window.location.host === url) {
-      fn().then((cleanUpFn) => cleanUpFn ? cleanUp = cleanUpFn : void 0);
+      const cleanUpFn = await fn();
+      if (cleanUpFn) cleanUp = cleanUpFn;
     }
   });
+};
+
+export const runOnReactPage = (url: string, fn: () => Removable) => {
+  let cleanUp: (() => void) | null = null;
+  let ignore = false;
+
+  const runner = async () => {
+    if (window.location.host !== url) return;
+    cleanUp?.();
+    ignore = true;
+    const cleanUpFn = await fn();
+    if (cleanUpFn) cleanUp = cleanUpFn;
+  };
+
+  const observer = new MutationObserver(() => {
+    if (window.location.host !== url) return;
+    if (ignore) {
+      ignore = false;
+      return;
+    }
+
+    runner();
+  });
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  window.navigation.addEventListener('navigate', runner);
+  observer.observe(document.body, { childList: true, subtree: true });
 };
 
 export const wait = (ms: number = 100) => new Promise(resolve => setTimeout(resolve, ms));
